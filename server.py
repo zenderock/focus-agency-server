8845e80bf6124fc0da446f7c48f50021401df5fa
Emmanuel Zenderock <149311683+zenderock@users.noreply.github.com>
2025-03-07 13:09:25 +0000

    add convertion

--- server.py
+++ server.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory, abort, send_file
 from flask_cors import CORS
 import os
+import subprocess
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
 UPLOAD_FOLDER = 'uploads'
+HLS_FOLDER = '/hls'
 ALLOWED_EXTENSIONS = {'mp4', 'avi', 'mov', 'wmv', 'flv'}
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024
@@ -46,7 +48,46 @@ def serve_video(user_id, filename):
     
     return send_file(file_path, conditional=True)
 
+
+@app.route('/upload', methods=['POST'])
+def upload_video():
+    file = request.files['video']  
+    if file:
+        filename = file.filename
+        input_path = os.path.join(UPLOAD_FOLDER, filename)
+        file.save(input_path)  
+
+        # Créer un dossier pour les segments HLS
+        hls_dir = os.path.join(HLS_FOLDER, filename.split('.')[0])
+        os.makedirs(hls_dir, exist_ok=True)
+        output_path = os.path.join(hls_dir, 'output.m3u8')
+
+        cmd = [
+            'ffmpeg',
+            '-i', input_path,
+            '-c:v', 'h264',
+            '-c:a', 'aac',
+            '-f', 'hls',
+            '-hls_time', '10',
+            '-hls_list_size', '0',
+            output_path
+        ]
+        try:
+            subprocess.run(cmd, check=True)  
+            # os.remove(input_path)
+            return {'message': 'Vidéo convertie en HLS', 'hls_path': output_path}, 200
+        except subprocess.CalledProcessError as e:
+            abort(500, description=f"Erreur lors de la conversion : {e}")
+    return {'message': 'Aucune vidéo uploadée'}, 400
+
+@app.route('/videos/<video_id>/playlist.m3u8')
+def serve_hls_playlist(video_id):
+    hls_dir = os.path.join(HLS_FOLDER, video_id)
+    playlist_path = os.path.join(hls_dir, 'output.m3u8')
+    return send_file(playlist_path, mimetype='application/x-mpegURL')
+
 if __name__ == '__main__':
     os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+    os.makedirs(HLS_FOLDER, exist_ok=True)
     app.run(debug=True, host='0.0.0.0', port=5000)
 
