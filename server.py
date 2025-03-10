c094ced87b81aa002da7f62428ecbfa6f92f4b55
Emmanuel Zenderock <149311683+zenderock@users.noreply.github.com>
2025-03-10 10:45:06 +0000

    remove GET mehod

--- server.py
+++ server.py
@@ -136,7 +136,7 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['POST'])
 def index():
     if request.method == 'POST':
         if 'video' not in request.files:
